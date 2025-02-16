c8f8ad89caedc43e88fa87c82707b0c2aa315161
Miguel Ángel <mbersan1005@g.educaand.es>
2025-02-16 13:00:18 +0100

    Editado archivo __manifesst__.py

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+module_info = {
+    'name': 'OpenWeatherMap Integration',
+    'version': '1.0',
+    'summary': 'Integración con la API de OpenWeatherMap',
+    'description': 'Obtención de datos meteorológicos en tiempo real.',
+    'author': 'Miguel Ángel Bernal Sánchez',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/weather_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+    'icon': ['/openweathermap_odoo/static/description/icon57.png'],
+}
